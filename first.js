9a2f939ea2a6be30decc9655b9af4f4d9f2c5ba1
Ajeet Singh <Ajeet@123gmail.com>
2026-02-07 12:01:24 +0530

    first commit

---
+++ first.js
@@ -0,0 +1,13 @@
+const name = "Rohit";
+
+const age = 90;
+const number = 4343224;
+const schId = 34343;
+
+const root = ()=>{
+    console.log("this is root function");
+}
+
+const pay = ()=>{
+    console.log("payment is added");
+}
